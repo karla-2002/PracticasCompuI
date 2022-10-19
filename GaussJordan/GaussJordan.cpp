56ddc86fdf34c2df389c0f8cb31cda263f0b1cea
karla-2002 <54778213+karla-2002@users.noreply.github.com>
2022-10-19 10:30:59 -0500

    Update GaussJordan.cpp

--- GaussJordan/GaussJordan.cpp
+++ GaussJordan/GaussJordan.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
 #include <array>
 
-// Añadir std para fácil llamado de funciones y para imprimir
+// Añadir std para fácil llamado de funciones
 using namespace std;
 
-//// Declaración de funciones:
+//// Declaración de funciones
 //Definimos un template (un tipo de dato) para nuestra matriz
-template <typename matriz> // define un tipo de variable llamado matriz y
-// que contendrá en nuestro caso la matriz sobre la que vamos a implementar el método de Gauss-Jordan
-void LlenarMatriz(matriz & miMatriz);// miMatriz está por referencia,
+template <typename matriz>
+void LlenarMatriz(matriz & miMatriz);
 
 template <typename matriz>
-void ImprimirMatriz (matriz & miMatriz); //Esta función me va a dejar imprimir la matriz en cualquier momento.   No la llames en el código final
+void ImprimirMatriz(matriz & miMatriz);
 
-template <typename matriz> //Este define
-//un tipo de variable llamado matriz y que contendrá en nuestro caso la matriz sobre la que vamos a implementar el método de Gauss-Jordan.
+template <typename matriz>
+void GaussJordan(matriz & miMatriz);
 
-void GaussJordan(matriz & miMatriz);//donde el argumento pasado por referencia miMatriz es la matriz a imprimir..
-// La funció Gauss jordan  tiene como objetivo implementar el algoritmo de Gauss-Jordan sobre una matriz de tamaño variablesXcolumnas.
-
-//
 template <typename matriz>
 void ImprimirSolucion(matriz & miMatriz);
 
+template <typename matriz>
+void multi(matriz & miMatriz);
+
+template <typename matriz>
+void cambio(matriz & miMatriz);
+
+template <typename matriz>
+void sum(matriz & miMatriz);
+
+template <typename matriz>
+void sum2(matriz & miMatriz);
 
 int main()
 {
     // Definimos el número de variables de nuestro sistema
-    const int variables = 3;// Para poder usarlo dentro del array debe de ser const int
+    const int variables = 3;
     // El número de columnas será el número de variables más su solición para cada ecuación
     const int columnas = variables + 1;
 
-    // Inicializamos la matriz que vamos a ocupar.
-    array <array<float, columnas>, variables> miMatriz = { 0 };// todos los valores de la matriz van a ser igua a 0
+    // Inicializamos la matriz que vamos a ocupar
+    array <array<float, columnas>, variables> miMatriz = { 0 };
 
-    // Pedimos al usuario que llene la matriz.
+    // Pedimos al usuario que llene la matriz
     LlenarMatriz(miMatriz);
 
     // Aplicamos el método de Gauss-Jordan sobre nuestra matriz
-    GaussJordan(miMatriz);
-
+    GaussJordan(miMatriz);// son las que tengo qe hacer
+    ImprimirMatriz(miMatriz);
     // Imprimimos la solución de la matriz
-    ImprimirSolucion(miMatriz);
+    ImprimirSolucion(miMatriz); //son las que tengo qe hacer
 
     return 0; // Indicamos que salimos del programa con éxito
 }
@@ -50,13 +56,13 @@ int main()
 Llena 'miMatriz' con valores ingresados por el usuario para cada elemento.
 No regresa ningún valor.
 */
-template <typename matriz>// para que el usuario llene la matriz se ocpa este ciclo for
+template <typename matriz>
 void LlenarMatriz(matriz & miMatriz)
 {
     int variables = miMatriz.size();
-    for (int i = 0; i < variables; i++) {  //para llenar la columna
-        for (int j = 0; j <= variables; j++) {    //para llenar la fila
-            cout << "Valor ele mento ["  << i <<  "]["  << j <<  "]: ";
+    for (int i = 0; i < variables; i++) {
+        for (int j = 0; j <= variables; j++) {
+            cout << "Valor elemento [" << i << "][" << j << "]: ";
             cin >> miMatriz[i][j];
         }
     }
@@ -67,7 +73,7 @@ Imprime cada elemento de 'miMatriz' emulando una matriz con corchetes cuadrados.
 No regresa ningún valor.
 */
 template <typename matriz>
-void ImprimirMatriz(matriz & miMatriz) // este for es para que aparezca el lugar del elemento de la matriz
+void ImprimirMatriz(matriz & miMatriz)
 {
     int variables = miMatriz.size();
     for (int i = 0; i < variables; i++) {
@@ -85,7 +91,7 @@ No regresa ningún valor.
 template <typename matriz>
 void ImprimirSolucion(matriz & miMatriz)
 {
-
+    cout<< "Solucion: " <<endl << "x0 = [" << miMatriz [0][3]<< "]" << endl << "x1 = [" << miMatriz [1][3]<< "]" <<endl <<"x2 = [" << miMatriz [2][3]<< "]"<<endl;
 }
 
 /*
@@ -95,7 +101,89 @@ No regresa ningún valor.
 template <typename matriz>
 void GaussJordan(matriz & miMatriz)
 {
-    //TODO
-    cout<< "Solucion: " <<endl << "x0 = [" << miMatriz [0][3]<< "]" << endl << "x1 = [" << miMatriz [1][3]<< "]" <<endl <<"x2 = [" << miMatriz [2][3]<< "]"<<endl;
+    cambio(miMatriz);
+    sum(miMatriz);
+    multi(miMatriz);
 
+
+
+}
+
+template <typename matriz>
+void multi(matriz & miMatriz){
+    int variables = miMatriz.size();
+    float factor = 0;
+        for (int i = 0; i < variables; i++) {
+        for (int j = 0; j < variables + 1; j++){
+            if (i==j && miMatriz[i][j] !=1 ){
+                factor=miMatriz[i][j];
+                for (int h=0 ; h < variables + 1; h++){
+                    miMatriz[i][h]= miMatriz[i][h]*(1/factor);
+                 }
+            }
+
+        }
+
+    }
+
+}
+
+template <typename matriz>// función para cambiar renglones
+void cambio(matriz & miMatriz){
+
+    int variables = miMatriz.size();
+    array <float, 4> Memoria={0};
+    for (int i = 0; i < variables; i++) {
+        for (int j = 0; j < variables + 1; j++){
+            if (i==j && miMatriz[i][j] ==0 ){
+                for (int h=0 ; h < variables + 1; h++){
+                    Memoria[h]= miMatriz[i+1][h];
+                    miMatriz [i+1][h]= miMatriz[i][h];
+                    miMatriz [i][h]= Memoria[h];
+                }
+            }
+
+        }
+
+    }
+}
+
+template <typename matriz>
+void sum(matriz & miMatriz){
+    int variables =miMatriz.size();
+    float a=0;
+    float b=0;
+    float x=0;
+    int m=0;
+    for (int j = 0; j < variables+1 ; j++) {
+        for (int i = 0; i < variables ; i++){
+            if (i==j){
+                m=i;
+                a= miMatriz[i][j];
+            }
+            if (i>j  &&  miMatriz[i][j] !=0 ){
+                b=miMatriz[i][j];
+                x=(-b)/a;
+                for (int c=0; c< variables+1; c++ ) {
+                    miMatriz[i][c] = (x * miMatriz[m][c]) + miMatriz[i][c];
+                }
+            }
+        }
+    }
+
+    for (int j = variables-1; j >=0 ; j--) {
+        for (int i = variables-1; i >= 0 ; i--){
+            if (i==j){
+                m=i;
+                a= miMatriz[i][j];
+            }
+            if (i<j  &&  miMatriz[i][j] !=0 ){
+                b=miMatriz[i][j];
+                x=(-b)/a;
+                for (int c=0; c< variables+1; c++ ) {
+                    miMatriz[i][c] = (x * miMatriz[m][c]) + miMatriz[i][c];
+                }
+            }
+        }
+    }
 }
